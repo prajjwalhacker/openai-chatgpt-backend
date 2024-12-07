36c3af8baa000501b7279d0d2abe75a17f921ef8
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-12-07 14:25:46 +0530

    server changes

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 const app = express();
 
 dotenv.config();
 
 
-app.use(cors());
+app.use(cors({
+}));
 
 app.get('/', (req ,res) => {
    res.send('hello world !');    
+})
+
+app.listen(8000, () => {
+  console.log("port is listening on port 8000");
 })
