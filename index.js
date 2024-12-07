dbc59d4e86137d2a584525afe443166fa8eb0273
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-12-07 18:35:22 +0530

    index changnes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const openai = require('openai');
 
 const app = express();
 
@@ -10,8 +11,39 @@ dotenv.config();
 app.use(cors({
 }));
 
-app.get('/', (req ,res) => {
+app.use(express.json());
+
+
+const configuration = new openai.Configuration(
+  {
+    apiKey: process.env.OPENAPI_API_KEY
+  }
+)
+
+app.get('/', async (req ,res) => {
    res.send('hello world !');    
+})
+
+app.post('/', async(req ,res) => {
+  try {
+      const response = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: 'hello',
+        temperature: 0,
+        max_tokens: 1000,
+        top_p: 1,
+        frequency_penalty: 0.5,
+        presense_penalty: 0
+      })
+      res.status(200).send({
+        bot: response.data.choices[0].text
+      });
+  }
+  catch (err) {
+    console.log("err");
+    console.log(err);
+    res.status(500).send("Something went wrong");
+  }
 })
 
 app.listen(8000, () => {
